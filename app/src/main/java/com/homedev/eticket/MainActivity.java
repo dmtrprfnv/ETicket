4a6a554d32d37f277d4317f9e61c2f784138addd
dmitry.20111@yandex.ru <9200470324asdf>
2016-03-13 20:36:11 +0300

    Add toolbar.

--- app/src/main/java/com/homedev/eticket/MainActivity.java
+++ app/src/main/java/com/homedev/eticket/MainActivity.java
@@ -1,13 +1,35 @@
 package com.homedev.eticket;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate (Bundle savedInstandState) {
         super.onCreate(savedInstandState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+        toolbar= (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
