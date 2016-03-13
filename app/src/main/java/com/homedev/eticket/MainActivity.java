810441fa7d11bb4d7a3e758db1149eb589c21b24
dmitry.20111@yandex.ru <9200470324asdf>
2016-03-13 18:58:45 +0300

    Create project [1].

---
+++ app/src/main/java/com/homedev/eticket/MainActivity.java
@@ -0,0 +1,13 @@
+package com.homedev.eticket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstandState) {
+        super.onCreate(savedInstandState);
+        setContentView(R.layout.activity_main);
+    }
+}
