fc6b2611e702d8df48c8eb9ebc654678c9042fa1
dmitry.20111@yandex.ru <9200470324asdf>
2016-03-16 00:01:32 +0300

    Add TabLayout + adapter + ViewPager, menu in NavigationView.

---
+++ app/src/main/java/com/homedev/eticket/Constants.java
@@ -0,0 +1,7 @@
+package com.homedev.eticket;
+
+public class Constants {
+
+    public static final int TAB_ONE =0;
+    public static final int TAB_TWO =1;
+}
--- app/src/main/java/com/homedev/eticket/MainActivity.java
+++ app/src/main/java/com/homedev/eticket/MainActivity.java
@@ -1,10 +1,18 @@
 package com.homedev.eticket;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.homedev.eticket.adapter.TabsPagerFragmentAdapter;
+import com.homedev.eticket.fragment.ExampleFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate (Bundle savedInstandState) {
@@ -21,12 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationView();
+        initTabs();
+
     }
 
     private void initToolbar() {
         toolbar= (Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
 
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -37,8 +48,40 @@ public class MainActivity extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.menu);
     }
 
+    private void initTabs() {
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
     private void initNavigationView() {
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()) {
+
+                    case R.id.actionMyRecords:
+                        showMyRecordsTab();
+
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showMyRecordsTab(){
+        viewPager.setCurrentItem(Constants.TAB_ONE);
 
     }
 }
